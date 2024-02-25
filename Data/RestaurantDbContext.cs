0e6f5df53aa66db95d39cabd74c4b2c816d7d823
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-02-24 19:43:04 -0500

    "Create Entity Framework database context"

---
+++ Data/RestaurantDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+//NOTE - Entity Framework database context
+
+namespace RestaurantRaterAPI.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : base(options) { }
+    }
+}
