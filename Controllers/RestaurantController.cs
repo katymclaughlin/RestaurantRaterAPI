c63ed3aed513996c87d7d4a51fa89c35a57390be
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-02-24 20:51:15 -0500

    "Added Microsoft Entity Framework version 5.0.12"

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantRaterAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 //This is where we will create API endpoints(CRUD methods to access our data)
 
@@ -17,6 +19,28 @@ namespace RestaurantRaterAPI.Controllers
         public RestaurantController(RestaurantDbContext context)
         {
         _context = context;
+        }
+    
+    //NOTE - READ (GET)
+    //Async Get Endpoint
+    [HttpGet]
+    public async Task<IActionResult> GetRestaurants()
+    {
+        List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+        return Ok(restaurants);
+    }
+
+    //NOTE - Retrieve a specific Restaurant using the Primary Key, or Id
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetRestaurantById(int id)
+    {
+        //fetch the Restaurant data from the database
+        Restaurant? restaurant = await _context.Restaurants.FindAsync(id);
+        if (restaurant is null)
+        {
+        return NotFound();
+        }
+        return Ok(restaurant);
         }
     }
 }
