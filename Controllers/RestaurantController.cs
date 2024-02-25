10ead773f3c06e731a81bf32a32bfa8aa54d2696
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-02-24 22:05:03 -0500

    "Created 'U' methods for CRUD "

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -20,9 +20,21 @@ namespace RestaurantRaterAPI.Controllers
         {
         _context = context;
         }
-    
+    //NOTE - CREATE a restaurant to the restaurants table
+    //Async POST Endpoint
+    [HttpPost]
+    public async Task<IActionResult> PostRestaurant([FromBody] Restaurant request)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Restaurants.Add(request);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        return BadRequest(ModelState);
+    }
     //NOTE - READ (GET)
-    //Async Get Endpoint
+    //Async GET Endpoint
     [HttpGet]
     public async Task<IActionResult> GetRestaurants()
     {
@@ -41,6 +53,68 @@ namespace RestaurantRaterAPI.Controllers
         return NotFound();
         }
         return Ok(restaurant);
+        }  
+
+    //NOTE - Update a restaurant
+    [HttpPut]
+    [Route("{id}")]
+    public async Task<IActionResult> UpdateRestaurant([FromForm] RestaurantEdit model, [FromRoute] int id)
+    {
+         var oldRestaurant = await _context.Restaurants.FindAsync(id);
+         if (oldRestaurant == null)
+         {
+            return NotFound();
+         }
+         if (!ModelState.IsValid)
+         {
+            return BadRequest();
+         }
+         if (!string.IsNullOrEmpty(model.Name))
+        {
+          oldRestaurant.Name = model.Name;
         }
+        if (!string.IsNullOrEmpty(model.Location))
+        {
+          oldRestaurant.Location = model.Location;
+        }
+            await _context.SaveChangesAsync();
+             return Ok();
+    }    
+    // Async PUT Endpoint
+[HttpPut]
+    public async Task<IActionResult> PutRestaurant([FromBody] Restaurant request)
+    {
+    if (!ModelState.IsValid)
+    {
+        return BadRequest(ModelState);
+    }
+
+    Restaurant? restaurant = await _context.Restaurants.FindAsync(request.Id);
+    if (restaurant is null)
+    {
+        return NotFound();
+    }
+    
+    restaurant.Name = request.Name;
+    restaurant.Location = request.Location;
+
+    _context.Restaurants.Update(restaurant);
+    await _context.SaveChangesAsync();
+    return Ok();
+    }
+    
+    //NOTE - Delete endpoint
+    public async Task<IActionResult> DeleteRestaurant([FromRoute] int id)
+    {
+        var restaurant = await _context.Restaurants.FindAsync(id);
+           if (restaurant == null)
+            {
+             return NotFound();
+            }
+            _context.Restaurants.Remove(restaurant);
+            await _context.SaveChangesAsync();
+            return Ok();
+    }
+   
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using RestaurantRaterApi.Data;
+using RestaurantRaterAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
